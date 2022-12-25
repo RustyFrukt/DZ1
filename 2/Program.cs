423bc4120ddedd8e946ffabdccf649341ade34ed
Frukt <88rum@mail.ru>
2022-12-25 21:37:29 +0300

    создали задачу 2

---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Берем числа до 100000
+
+System.Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+if (number.Length < 3)
+{
+    System.Console.WriteLine("Внимание! Указанное число содержит менее 3х знаков");
+}
+else
+{
+    System.Console.WriteLine(number[2]);
+}
